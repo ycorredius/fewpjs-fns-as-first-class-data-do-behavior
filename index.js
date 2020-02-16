fae01a905b2fa3f7cfa6faf342ebaf8a1b8a9ec8
shaun <ycorredius@gmail.com>
2020-02-16 04:24:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById('time').value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let message = "Good Morning";
+  let hourInt = timeString.split(':')[0];
+  parseInt(hourInt);
+  if (hourInt < 12){
+  } else if (hourInt > 17) {
+    message = "Good Evening";
+  } else {
+    message = "Good Afternoon";
+  }
+  return message;
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
